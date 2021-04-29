d4d29ba204657aa7a444cadd4ab70c8879c0a1bb
savannahvaith <sav.vaithilingam@gmail.com>
2021-04-29 15:40:53 +0100

    SV: Added promises example

--- JavaScript/06-Callbacks-And-Promises.js
+++ JavaScript/06-Callbacks-And-Promises.js
@@ -2,7 +2,7 @@
 
 // Callback = function that takes in another function as a parameter
 
-const greeting = (firstName) =>{
+const greeting = (firstName) => {
     // creating a pop up with someones name printed inside
     alert(`Hello ${firstName}`);
 }
@@ -18,16 +18,16 @@ const writeSomething = (function1) => {
 // Higher order functions
 
 let grades = [
-    {"name":"Bob", "grade":8, "gender":"M"},
-    {"name":"Molly", "grade":12, "gender":"F"},
-    {"name":"Jim", "grade":34, "gender":"M"},
-    {"name":"Poppy", "grade":8, "gender":"F"},
-    {"name":"Bob", "grade":56, "gender":"M"},
-    {"name":"Bob", "grade":33, "gender":"M"},
-    {"name":"Bob", "grade":12, "gender":"M"},
-    {"name":"Billy", "grade":8, "gender":"F"},
-    {"name":"Mary", "grade":8, "gender":"F"},
-    {"name":"Poppins", "grade":8, "gender":"F"},
+    { "name": "Bob", "grade": 8, "gender": "M" },
+    { "name": "Molly", "grade": 12, "gender": "F" },
+    { "name": "Jim", "grade": 34, "gender": "M" },
+    { "name": "Poppy", "grade": 8, "gender": "F" },
+    { "name": "Bob", "grade": 56, "gender": "M" },
+    { "name": "Bob", "grade": 33, "gender": "M" },
+    { "name": "Bob", "grade": 12, "gender": "M" },
+    { "name": "Billy", "grade": 8, "gender": "F" },
+    { "name": "Mary", "grade": 8, "gender": "F" },
+    { "name": "Poppins", "grade": 8, "gender": "F" },
 ]
 
 // The average grade of the classroom 
@@ -52,11 +52,6 @@ console.log(getGirls(grades));
 
 
 
-
-
-
-
-
 // Commitment - something you said you were giong to do 
 // fulfill it, break it, pending/forget
 
@@ -66,4 +61,52 @@ available immediately, some time in the future,
 possibly not at all 
 */
 
-// let prompom = new Promise()
+const hello = () => { }; //<- named function
+() => { }; //<- annonymous function
+
+let prompom = new Promise((resolve, reject) => {
+    let a = 1 + 1;
+    if (a == 2) {
+        resolve("Successful yay!"); // successful outcome
+    } else {
+        reject("error oh no :( "); // failed outcome
+    }
+});
+
+// if success .then() we want do something
+// else if rejected .catch()
+
+// prompom.then( () => {
+//     console.log("Yay we executed with no errors");
+// }).catch( (msg) => {
+//     console.error(`Something went terribly wrong! ${msg}`);
+// });
+
+// order of execution goes:
+// prompom.then(() => {
+//     console.log("This will execute for a RESOLVE() method");
+// }).catch((msg) => {
+//     console.error(`This will execute for a REJECT() method`);
+// }).then(() => {
+//     console.log(`I will execute, regardless of the outcome ^`);
+// });
+
+let newPromise = new Promise( (resolve, reject) => {
+    // complicated logic
+    console.log(`initial`);
+    resolve(); 
+    // reject(); // unreachable code
+}).then( () => {
+    console.log(`Do this`);
+}).then( () => {
+    throw new Error("Something has failed"); 
+    console.log(`Straight after`);
+}).catch( () => {
+    console.log(`oh noooo`);
+})
+
+//? initial, do this, straight after
+
+//? initial, oh no 
+
+//? initial, do this, oh no 
